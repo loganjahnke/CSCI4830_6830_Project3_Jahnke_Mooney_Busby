2b99962bc2b61a5b80df26da636f94f65d8c1195
Matthew Mooney <33425006+mooneymatthew@users.noreply.github.com>
2019-04-17 11:21:51 -0400

    Beginnings of excavator

--- Project3/Assets/Scripts/CraneRotate.cs
+++ Project3/Assets/Scripts/CraneRotate.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CraneRotate : MonoBehaviour
 {
+    public GameObject arm;
+    public GameObject arm2;
+    public GameObject bucket;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,21 +16,37 @@ public class CraneRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //move up
-        if(Input.GetKey(KeyCode.W)) {
-            transform.Rotate(0,0,1,Space.Self);
+        //move arm down
+        if(Input.GetKey(KeyCode.DownArrow)) {
+            arm.transform.Rotate(1,0,0,Space.Self);
         }
         //move left
         if (Input.GetKey(KeyCode.A)) {
             transform.Rotate(0,-1,0,Space.World);
         }
-        //move down
-        if (Input.GetKey(KeyCode.S)) {
-            transform.Rotate(0,0,-1,Space.Self);
+        //move arm up
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            arm.transform.Rotate(-1,0,0,Space.Self);
         }
         //move up
         if (Input.GetKey(KeyCode.D)) {
             transform.Rotate(0,1,0,Space.World);
+        }
+        //arm2 angle out
+        if (Input.GetKey(KeyCode.S)) {
+            arm2.transform.Rotate(1, 0, 0, Space.Self);
+        }
+        //arm2 angle in
+        if (Input.GetKey(KeyCode.W)) {
+            arm2.transform.Rotate(-1, 0, 0, Space.Self);
+        }
+        //bucket close
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            bucket.transform.Rotate(1, 0, 0, Space.Self);
+        }
+        //bucket open
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            bucket.transform.Rotate(-1, 0, 0, Space.Self);
         }
     }
 }
