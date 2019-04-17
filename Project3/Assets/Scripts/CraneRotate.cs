cefed2be802f258a9931f25348bb04b65653f35b
Matthew Mooney <33425006+mooneymatthew@users.noreply.github.com>
2019-04-16 22:25:16 -0400

    wrecking ball stuff

---
+++ Project3/Assets/Scripts/CraneRotate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraneRotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move up
+        if(Input.GetKey(KeyCode.W)) {
+            transform.Rotate(0,0,1,Space.Self);
+        }
+        //move left
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(0,-1,0,Space.World);
+        }
+        //move down
+        if (Input.GetKey(KeyCode.S)) {
+            transform.Rotate(0,0,-1,Space.Self);
+        }
+        //move up
+        if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(0,1,0,Space.World);
+        }
+    }
+}
