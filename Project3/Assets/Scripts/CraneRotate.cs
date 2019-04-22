3b1d73342217e816c3cf134700b2e2f147227a01
Matthew Mooney <33425006+mooneymatthew@users.noreply.github.com>
2019-04-22 18:47:36 -0400

    Implemented dirt to pick up

--- Project3/Assets/Scripts/CraneRotate.cs
+++ Project3/Assets/Scripts/CraneRotate.cs
@@ -7,6 +7,7 @@ public class CraneRotate : MonoBehaviour
     public GameObject arm;
     public GameObject arm2;
     public GameObject bucket;
+    public float vel;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,38 +18,38 @@ public class CraneRotate : MonoBehaviour
     void Update()
     {
         //move arm down
-        if(Input.GetKey(KeyCode.DownArrow) && arm.transform.localRotation.x < 0.2) {
+        if(Input.GetKey(KeyCode.DownArrow) && arm.transform.localRotation.x < 0.3) {
             Debug.Log(arm.transform.localRotation.x);
-            arm.transform.Rotate(1,0,0,Space.Self);
+            arm.transform.Rotate((vel * 0.5f),0,0,Space.Self);
         }
         //move left
         if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(0,-1,0,Space.World);
+            transform.Rotate(0,-(vel * 0.5f), 0,Space.World);
         }
         //move arm up
         if (Input.GetKey(KeyCode.UpArrow) && arm.transform.localRotation.x > -0.2) {
             Debug.Log(arm.transform.localRotation.x);
-            arm.transform.Rotate(-1,0,0,Space.Self);
+            arm.transform.Rotate(-(vel * 0.5f), 0,0,Space.Self);
         }
         //move right
         if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(0,1,0,Space.World);
+            transform.Rotate(0, (vel * 0.5f), 0,Space.World);
         }
         //arm2 angle in
         if (Input.GetKey(KeyCode.S) && arm2.transform.localRotation.x < 0.5) {
-            arm2.transform.Rotate(1, 0, 0, Space.Self);
+            arm2.transform.Rotate(vel, 0, 0, Space.Self);
         }
         //arm2 angle out
         if (Input.GetKey(KeyCode.W) && arm2.transform.localRotation.x > -0.6) {
-            arm2.transform.Rotate(-1, 0, 0, Space.Self);
+            arm2.transform.Rotate(-vel, 0, 0, Space.Self);
         }
         //bucket close
         if (Input.GetKey(KeyCode.LeftArrow) && bucket.transform.localRotation.x < 0.7) {
-            bucket.transform.Rotate(1, 0, 0, Space.Self);
+            bucket.transform.Rotate(vel, 0, 0, Space.Self);
         }
         //bucket open
         if (Input.GetKey(KeyCode.RightArrow) && bucket.transform.localRotation.x > -.65) {
-            bucket.transform.Rotate(-1, 0, 0, Space.Self);
+            bucket.transform.Rotate(-vel, 0, 0, Space.Self);
         }
     }
 }
---
+++ Project3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject DirtPrefab;
+    public Transform DirtSpawner;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 500; i++) { 
+            Instantiate(DirtPrefab, DirtSpawner.position, new Quaternion(0, 0, 0, 0));
+            DirtSpawner.position += new Vector3(0, 0.5f, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
