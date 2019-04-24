23a5744abafb7feb9663977194f4ca0be20f7d12
Logan Jahnke <ljahnke9@gmail.com>
2019-04-24 11:15:00 -0400

    progress bars for dirt

---
+++ Project3/Assets/Scripts/HoleCounter.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleCounter : MonoBehaviour
+{
+
+    public GameObject progressBar;
+
+    public int dirt = 0;
+    public float max = 30f;
+
+    float mfX;
+    float mfY;
+    float mfZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.mfX = this.progressBar.transform.position.x - this.progressBar.transform.localScale.x / 2.0f;
+        this.mfY = this.progressBar.transform.position.y - this.progressBar.transform.localScale.y / 2.0f;
+        this.mfZ = this.progressBar.transform.position.z - this.progressBar.transform.localScale.z / 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v3Scale = this.progressBar.transform.localScale;
+        this.progressBar.transform.localScale = new Vector3(10f, this.max * (this.dirt / 500f), 10f);
+        this.progressBar.transform.position = new Vector3(this.progressBar.transform.position.x, this.mfY + this.progressBar.transform.localScale.y / 2.0f, this.progressBar.transform.position.z);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this.dirt++;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this.dirt--;
+    }
+
+}
---
+++ Project3/Assets/Scripts/MissingCounter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissingCounter : MonoBehaviour
+{
+    public GameObject start;
+    public GameObject goal;
+
+    public int dirt = 0;
+    public float max = 30f;
+
+    float mfX;
+    float mfY;
+    float mfZ;
+
+    private HoleCounter startCounter;
+    private HoleCounter goalCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.mfX = this.transform.position.x - this.transform.localScale.x / 2.0f;
+        this.mfY = this.transform.position.y - this.transform.localScale.y / 2.0f;
+        this.mfZ = this.transform.position.z - this.transform.localScale.z / 2.0f;
+
+        this.startCounter = this.start.GetComponent<HoleCounter>();
+        this.goalCounter = this.goal.GetComponent<HoleCounter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.dirt = 500 - this.startCounter.dirt - this.goalCounter.dirt;
+        Vector3 v3Scale = this.transform.localScale;
+        this.transform.localScale = new Vector3(10f, this.max * (this.dirt / 500f), 10f);
+        this.transform.position = new Vector3(this.transform.position.x, this.mfY + this.transform.localScale.y / 2.0f, this.transform.position.z);
+    }
+}
