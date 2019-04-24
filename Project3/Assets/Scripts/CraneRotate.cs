771f3685731c0f16f69ac8632bcc80392555c28c
Matthew Mooney <mgm36355@uga.edu>
2019-04-24 12:27:45 -0400

    VR controls

--- Project3/Assets/Scripts/CraneRotate.cs
+++ Project3/Assets/Scripts/CraneRotate.cs
@@ -8,6 +8,8 @@ public class CraneRotate : MonoBehaviour
     public GameObject arm2;
     public GameObject bucket;
     public float vel;
+    public Transform leftController;
+    public Transform rightController;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,37 +20,35 @@ public class CraneRotate : MonoBehaviour
     void Update()
     {
         //move arm down
-        if(Input.GetKey(KeyCode.DownArrow) && arm.transform.localRotation.x < 0.3) {
-            Debug.Log(arm.transform.localRotation.x);
+        if ((rightController.localRotation.x > -.65f && rightController.localRotation.x < 0.7f) && arm.transform.localRotation.x < 0.3) {
             arm.transform.Rotate((vel * 0.5f),0,0,Space.Self);
         }
         //move left
-        if (Input.GetKey(KeyCode.A)) {
+        if (leftController.localRotation.z > 0.05f) {
             transform.Rotate(0,-(vel * 0.5f), 0,Space.World);
         }
         //move arm up
-        if (Input.GetKey(KeyCode.UpArrow) && arm.transform.localRotation.x > -0.2) {
-            Debug.Log(arm.transform.localRotation.x);
+        if ((rightController.localRotation.x < -.75f || rightController.localRotation.x > 0.7f) && arm.transform.localRotation.x > -0.2) {
             arm.transform.Rotate(-(vel * 0.5f), 0,0,Space.Self);
         }
         //move right
-        if (Input.GetKey(KeyCode.D)) {
+        if (leftController.localRotation.z < -0.07f) {
             transform.Rotate(0, (vel * 0.5f), 0,Space.World);
         }
         //arm2 angle in
-        if (Input.GetKey(KeyCode.S) && arm2.transform.localRotation.x < 0.5) {
+        if ((leftController.localRotation.x < -.75f || leftController.localRotation.x > 0.7f) && arm2.transform.localRotation.x < 0.5) {
             arm2.transform.Rotate(vel, 0, 0, Space.Self);
         }
         //arm2 angle out
-        if (Input.GetKey(KeyCode.W) && arm2.transform.localRotation.x > -0.6) {
+        if ((leftController.localRotation.x > -.65f && leftController.localRotation.x < 0.7f) && arm2.transform.localRotation.x > -0.6) {
             arm2.transform.Rotate(-vel, 0, 0, Space.Self);
         }
         //bucket close
-        if (Input.GetKey(KeyCode.LeftArrow) && bucket.transform.localRotation.x < 0.7) {
+        if (rightController.localRotation.z > 0.05f && bucket.transform.localRotation.x < 0.7) {
             bucket.transform.Rotate(vel, 0, 0, Space.Self);
         }
         //bucket open
-        if (Input.GetKey(KeyCode.RightArrow) && bucket.transform.localRotation.x > -.65) {
+        if (rightController.localRotation.z < -0.07f && bucket.transform.localRotation.x > -.65) {
             bucket.transform.Rotate(-vel, 0, 0, Space.Self);
         }
     }
